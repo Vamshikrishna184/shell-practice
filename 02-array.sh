ebd16ef7b7256b9861960d2dbdc32a93ef829d60
Vamshi <vamsh@LAPTOP-6JI808O6>
2025-05-20 20:17:27 +0530

    Hello World

--- 02-array.sh
+++ 02-array.sh
@@ -1,5 +1,8 @@
+echo "Vamshi in love" 
 #!/bin/bash
 
-MOVIES=("court" "Hit" "Pushpa2")
 
-echo "First movie is : $MOVIES[0]
+MOVIES=("court" "Hit" "Pushpa2" 'Love')
+
+echo "First movie is : $MOVIES[0]"
+
